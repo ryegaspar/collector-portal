859f293f221574825e543c53765f9349722756e4
Ryan Gaspar <rgaspar@UNIFININC.COM>
2018-10-05 14:12:08 -0500

    ready for v1

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -21,60 +21,60 @@ class AdminSeeder extends Seeder
 
         $user->syncRoles('super-admin');
 
-        factory(Admin::class)->create([
-            'username' => 'admin2',
-            'password' => bcrypt('Password'),
-        ]);
-
-        $manager = factory(Admin::class)->create([
-            'username' => 'manager',
-            'password' => bcrypt('Password')
-        ]);
-
-        $manager->syncRoles('manager');
-
-        $site_manager = factory(Admin::class)->create([
-            'username' => 'sitemanager',
-            'password' => bcrypt('Password'),
-            'site_id'  => 1
-        ]);
-
-        $site_manager->syncRoles('site-manager');
-
-        $sub_site_manager = factory(Admin::class)->create([
-            'username'    => 'subsitemanager',
-            'password'    => bcrypt('Password'),
-            'site_id'     => 2,
-            'sub_site_id' => 2,
-        ]);
-
-        $sub_site_manager->syncRoles('sub-site-manager');
-
-        $teamleader1 = factory(Admin::class)->create([
-            'username'    => 'teamleader',
-            'password'    => bcrypt('Password'),
-            'site_id'     => 2,
-            'sub_site_id' => 2,
-        ]);
-
-        $teamleader1->syncRoles('team-leader');
-
-        $teamleader2 = factory(Admin::class)->create([
-            'username'    => 'teamleader2',
-            'password'    => bcrypt('Password'),
-            'site_id'     => 2,
-            'sub_site_id' => 3,
-        ]);
-
-        $teamleader2->syncRoles('team-leader');
-
-        $teamleader3 = factory(Admin::class)->create([
-            'username'    => 'teamleader3',
-            'password'    => bcrypt('Password'),
-            'site_id'     => 2,
-            'sub_site_id' => 3,
-        ]);
-
-        $teamleader3->syncRoles('team-leader');
+//        factory(Admin::class)->create([
+//            'username' => 'admin2',
+//            'password' => bcrypt('Password'),
+//        ]);
+//
+//        $manager = factory(Admin::class)->create([
+//            'username' => 'manager',
+//            'password' => bcrypt('Password')
+//        ]);
+//
+//        $manager->syncRoles('manager');
+//
+//        $site_manager = factory(Admin::class)->create([
+//            'username' => 'sitemanager',
+//            'password' => bcrypt('Password'),
+//            'site_id'  => 1
+//        ]);
+//
+//        $site_manager->syncRoles('site-manager');
+//
+//        $sub_site_manager = factory(Admin::class)->create([
+//            'username'    => 'subsitemanager',
+//            'password'    => bcrypt('Password'),
+//            'site_id'     => 2,
+//            'sub_site_id' => 2,
+//        ]);
+//
+//        $sub_site_manager->syncRoles('sub-site-manager');
+//
+//        $teamleader1 = factory(Admin::class)->create([
+//            'username'    => 'teamleader',
+//            'password'    => bcrypt('Password'),
+//            'site_id'     => 2,
+//            'sub_site_id' => 2,
+//        ]);
+//
+//        $teamleader1->syncRoles('team-leader');
+//
+//        $teamleader2 = factory(Admin::class)->create([
+//            'username'    => 'teamleader2',
+//            'password'    => bcrypt('Password'),
+//            'site_id'     => 2,
+//            'sub_site_id' => 3,
+//        ]);
+//
+//        $teamleader2->syncRoles('team-leader');
+//
+//        $teamleader3 = factory(Admin::class)->create([
+//            'username'    => 'teamleader3',
+//            'password'    => bcrypt('Password'),
+//            'site_id'     => 2,
+//            'sub_site_id' => 3,
+//        ]);
+//
+//        $teamleader3->syncRoles('team-leader');
     }
 }
