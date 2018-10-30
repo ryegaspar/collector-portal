6a118122b47db630fc60e10307ac354b91390e03
Andrew Slade <aslade@unifinrs.com>
2018-10-30 23:58:41 +0000

    Test Data for TodaysTotals

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,4 +10,27 @@
 			<dashboard :Totals="{{ $todaysTotals }}" class="mt-md-4"></dashboard>
 		</div>
 	</main>
+	
+	<table>
+		<tr>
+			<td>Group</td>
+			<td>GoodForToday</td>
+			<td>CurrentMonth</td>
+			<td>NextMonth</td>
+			<td>ThirtyDay</td>
+			<td>NinetyDay</td>
+			<td>AllIn</td>
+		</tr>
+		@foreach($todaysTotals as $value)
+			<tr>
+				<td>{{ $value-> UGP.UGP_DESC}}</td>
+				<td>{{ $value-> GFT}}</td>
+				<td>{{ $value-> CurrentMonth}}</td>
+				<td>{{ $value-> NextMonth}}</td>
+				<td>{{ $value-> 30Day}}</td>
+				<td>{{ $value-> 90Day}}</td>
+				<td>{{ $value-> AllIn}}</td>
+			</tr>
+		@endforeach
+	</table>
 @endsection
