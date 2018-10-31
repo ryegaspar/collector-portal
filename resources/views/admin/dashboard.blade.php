a217616d4d73f8d67df2277c16b7a1b01d5439b7
Andrew Slade <aslade@unifinrs.com>
2018-10-31 00:05:47 +0000

    test (fix)

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,28 +9,31 @@
 		<div class="container-fluid">
 			<dashboard :Totals="{{ $todaysTotals }}" class="mt-md-4"></dashboard>
 		</div>
+		<div class="container-fluid">
+			<table>
+				<tr>
+					<td>Group</td>
+					<td>GoodForToday</td>
+					<td>CurrentMonth</td>
+					<td>NextMonth</td>
+					<td>ThirtyDay</td>
+					<td>NinetyDay</td>
+					<td>AllIn</td>
+				</tr>
+				@foreach($todaysTotals as $value)
+					<tr>
+						<td>{{ $value-> UGP.UGP_DESC}}</td>
+						<td>{{ $value-> GFT}}</td>
+						<td>{{ $value-> CurrentMonth}}</td>
+						<td>{{ $value-> NextMonth}}</td>
+						<td>{{ $value-> 30Day}}</td>
+						<td>{{ $value-> 90Day}}</td>
+						<td>{{ $value-> AllIn}}</td>
+					</tr>
+				@endforeach
+			</table>
+		</div>
+
 	</main>
-	
-	<table>
-		<tr>
-			<td>Group</td>
-			<td>GoodForToday</td>
-			<td>CurrentMonth</td>
-			<td>NextMonth</td>
-			<td>ThirtyDay</td>
-			<td>NinetyDay</td>
-			<td>AllIn</td>
-		</tr>
-		@foreach($todaysTotals as $value)
-			<tr>
-				<td>{{ $value-> UGP.UGP_DESC}}</td>
-				<td>{{ $value-> GFT}}</td>
-				<td>{{ $value-> CurrentMonth}}</td>
-				<td>{{ $value-> NextMonth}}</td>
-				<td>{{ $value-> 30Day}}</td>
-				<td>{{ $value-> 90Day}}</td>
-				<td>{{ $value-> AllIn}}</td>
-			</tr>
-		@endforeach
-	</table>
+
 @endsection
