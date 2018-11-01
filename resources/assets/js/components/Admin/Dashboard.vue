051898ec5a295e237f1c91f4bb9fe007a5abf2bc
Ryan Gaspar <rgaspar@UNIFININC.COM>
2018-11-01 12:26:42 -0500

    admin - dashboard - add total to summary

--- resources/assets/js/components/Admin/Dashboard.vue
+++ resources/assets/js/components/Admin/Dashboard.vue
@@ -23,6 +23,15 @@
                                 <tr v-for="item in summary">
                                     <td v-for="(place, index) in item" :class="getClass(index)">{{ toNumber(place, index) }}</td>
                                 </tr>
+                                <tr>
+                                    <td><strong>TOTAL</strong></td>
+                                    <td class="text-right" v-text="toNumber(totalToday)"></td>
+                                    <td class="text-right" v-text="toNumber(totalCurrentMonth)"></td>
+                                    <td class="text-right" v-text="toNumber(totalNextMonth)"></td>
+                                    <td class="text-right" v-text="toNumber(totalNext30)"></td>
+                                    <td class="text-right" v-text="toNumber(totalNext120)"></td>
+                                    <td class="text-right" v-text="toNumber(totalAll)"></td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -37,7 +46,7 @@
 		props: ['summary'],
 
 		methods: {
-			toNumber(value, index) {
+			toNumber(value, index = '') {
 				if (index === 'name')
 					return value;
 
@@ -46,8 +55,41 @@
 
 			getClass(index) {
 				return (index !== 'name' ? 'text-right' : '');
-			}
-		}
+			},
 
+			getTotalSummary(column) {
+				let total = 0;
+				(this.summary).forEach((obj, index) => {
+					total +=parseFloat(obj[column]);
+				});
+				return total;
+			},
+		},
+
+        computed: {
+			totalToday() {
+				return this.getTotalSummary('today');
+            },
+
+            totalCurrentMonth() {
+				return this.getTotalSummary('currentMonth');
+            },
+
+            totalNextMonth() {
+				return this.getTotalSummary('nextMonth');
+            },
+
+            totalNext30() {
+				return this.getTotalSummary('next30');
+            },
+
+            totalNext120() {
+				return this.getTotalSummary('next120');
+            },
+
+            totalAll() {
+				return this.getTotalSummary('all');
+            }
+        }
 	}
 </script>
