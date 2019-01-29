6010d1684bc523b376b049acfeee7f7f273d962c
Ryan Gaspar <rgaspar@UNIFININC.COM>
2019-01-29 14:40:15 -0600

    add bar graph (admin dashboard)

--- resources/assets/js/components/Admin/Dashboard.vue
+++ resources/assets/js/components/Admin/Dashboard.vue
@@ -21,11 +21,19 @@
 
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
+                    <div class="card-header card-header-flex">
                         <h4 class="card-title">Today's Total (Data)</h4>
+                        <button class="btn btn-info rounded" @click="redirectTodaysTotals()">Go to Detailed Totals</button>
                     </div>
                     <div class="card-body">
-                        <table class="table table-responsive-sm table-bordered table-striped">
+                        <table class="table table-responsive-sm table-striped table-bordered">
+                            <colgroup>
+                                <col>
+                                <col span="3" style="background-color: lightcyan">
+                                <col span="3" style="background-color: lavender">
+                                <col span="3" style="background-color: lightcyan">
+                                <col span="3" style="background-color: lavender">
+                            </colgroup>
                             <thead>
                                 <tr>
                                     <th>Collection Group</th>
@@ -49,30 +57,30 @@
                                     <td class="text-right">{{ r_data.gft_total }}</td>
                                     <td class="text-center">{{ r_data.gft_count }}</td>
                                     <td class="text-right">{{ r_data.gft_avg }}</td>
-                                    <td class="text-right">{{ r_data.this_month_total }}</td>
-                                    <td class="text-center">{{ r_data.this_month_count }}</td>
-                                    <td class="text-right">{{ r_data.this_month_avg }}</td>
+                                    <td class="text-right bg-lavender">{{ r_data.this_month_total }}</td>
+                                    <td class="text-center bg-lavender">{{ r_data.this_month_count }}</td>
+                                    <td class="text-right bg-lavender">{{ r_data.this_month_avg }}</td>
                                     <td class="text-right">{{ r_data.thirty_days_total }}</td>
                                     <td class="text-center">{{ r_data.thirty_days_count}}</td>
                                     <td class="text-right">{{ r_data.thirty_days_avg }}</td>
-                                    <td class="text-right">{{ r_data.all_total}}</td>
-                                    <td class="text-center">{{ r_data.all_count}}</td>
-                                    <td class="text-right">{{ r_data.all_avg}}</td>
+                                    <td class="text-right bg-lavender">{{ r_data.all_total}}</td>
+                                    <td class="text-center bg-lavender">{{ r_data.all_count}}</td>
+                                    <td class="text-right bg-lavender">{{ r_data.all_avg}}</td>
                                 </tr>
                                 <tr>
                                     <td><strong>TOTAL</strong></td>
                                     <td class="text-right">{{ sum(gft, 'total').toFixed(2) }}</td>
                                     <td class="text-center">{{ sum(gft, 'count') }}</td>
                                     <td class="text-center">{{ sum(gft, 'avg').toFixed(2) }}</td>
-                                    <td class="text-right">{{ sum(thisMonth, 'total').toFixed(2) }}</td>
-                                    <td class="text-center">{{ sum(thisMonth, 'count') }}</td>
-                                    <td class="text-center">{{ sum(thisMonth, 'avg').toFixed(2) }}</td>
+                                    <td class="text-right bg-lavender">{{ sum(thisMonth, 'total').toFixed(2) }}</td>
+                                    <td class="text-center bg-lavender">{{ sum(thisMonth, 'count') }}</td>
+                                    <td class="text-center bg-lavender">{{ sum(thisMonth, 'avg').toFixed(2) }}</td>
                                     <td class="text-right">{{ sum(thirtyDays, 'total').toFixed(2) }}</td>
                                     <td class="text-center">{{ sum(thirtyDays, 'count') }}</td>
                                     <td class="text-center">{{ sum(thirtyDays, 'avg').toFixed(2) }}</td>
-                                    <td class="text-right">{{ sum(all, 'total').toFixed(2) }}</td>
-                                    <td class="text-center">{{ sum(all, 'count') }}</td>
-                                    <td class="text-center">{{ sum(all, 'avg').toFixed(2) }}</td>
+                                    <td class="text-right bg-lavender">{{ sum(all, 'total').toFixed(2) }}</td>
+                                    <td class="text-center bg-lavender">{{ sum(all, 'count') }}</td>
+                                    <td class="text-center bg-lavender">{{ sum(all, 'avg').toFixed(2) }}</td>
                                 </tr>
                             </tbody>
                         </table>
@@ -81,126 +89,126 @@
             </div>
 
         </div>
-        <div class="row" v-show="hasData">
 
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Today's Total (Data)</h4>
-                    </div>
-                    <div class="card-body">
-                        <table class="table table-responsive-sm table-bordered table-striped">
-                            <thead>
-                            <tr>
-                                <th>&nbsp;</th>
-                                <th v-for="group in groups" class="text-right">{{ group }}</th>
-                                <th class="text-right">Totals</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td class><strong>GFT Total Amount</strong></td>
-                                <td v-for="data_gft in gft" class="text-right">{{ data_gft.total }}</td>
-                                <td class="text-right">{{ sum(gft, 'total').toFixed(2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>GFT Count</td>
-                                <td v-for="data_gft in gft" class="text-right">{{ data_gft.count}}</td>
-                                <td class="text-right">{{ sum(gft, 'count') }}</td>
-                            </tr>
-                            <tr>
-                                <td>GFT Avg</td>
-                                <td v-for="data_gft in gft" class="text-right">{{ data_gft.avg}}</td>
-                                <td class="text-right">{{ sum(gft, 'avg').toFixed(2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>This Month Total Amount</td>
-                                <td v-for="data_thisMonth in thisMonth" class="text-right">{{ data_thisMonth.total}}
-                                </td>
-                                <td class="text-right">{{ sum(thisMonth, 'total').toFixed(2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>This Month Count</td>
-                                <td v-for="data_thisMonth in thisMonth" class="text-right">{{ data_thisMonth.count}}
-                                </td>
-                                <td class="text-right">{{ sum(thisMonth, 'count') }}</td>
-                            </tr>
-                            <tr>
-                                <td>This Month Avg</td>
-                                <td v-for="data_thisMonth in thisMonth" class="text-right">{{ data_thisMonth.avg}}</td>
-                                <td class="text-right">{{ sum(thisMonth, 'avg').toFixed(2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>30 Days Total Amount</td>
-                                <td v-for="data_thirtyDays in thirtyDays" class="text-right">{{
-                                    data_thirtyDays.total}}
-                                </td>
-                                <td class="text-right">{{ sum(thirtyDays, 'total').toFixed(2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>30 Days Count</td>
-                                <td v-for="data_thirtyDays in thirtyDays" class="text-right">{{
-                                    data_thirtyDays.count}}
-                                </td>
-                                <td class="text-right">{{ sum(thirtyDays, 'count') }}</td>
-                            </tr>
-                            <tr>
-                                <td>30 Days Avg</td>
-                                <td v-for="data_thirtyDays in thirtyDays" class="text-right">{{ data_thirtyDays.avg}}
-                                </td>
-                                <td class="text-right">{{ sum(thirtyDays, 'avg').toFixed(2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>All Total Amount</td>
-                                <td v-for="data_all in all" class="text-right">{{ data_all.total}}</td>
-                                <td class="text-right">{{ sum(all, 'total').toFixed(2) }}</td>
-                            </tr>
-                            <tr>
-                                <td>All Count</td>
-                                <td v-for="data_all in all" class="text-right">{{ data_all.count}}</td>
-                                <td class="text-right">{{ sum(all, 'count') }}</td>
-                            </tr>
-                            <tr>
-                                <td>All Avg</td>
-                                <td v-for="data_all in all" class="text-right">{{ data_all.avg}}</td>
-                                <td class="text-right">{{ sum(all, 'avg').toFixed(2) }}</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+        <!--data flipped-->
+        <!--<div class="row" v-show="hasData">-->
+            <!--<div class="col-md-12">-->
+                <!--<div class="card">-->
+                    <!--<div class="card-header">-->
+                        <!--<h4 class="card-title">Today's Total (Data)</h4>-->
+                    <!--</div>-->
+                    <!--<div class="card-body">-->
+                        <!--<table class="table table-responsive-sm table-bordered table-striped">-->
+                            <!--<thead>-->
+                            <!--<tr>-->
+                                <!--<th>&nbsp;</th>-->
+                                <!--<th v-for="group in groups" class="text-right">{{ group }}</th>-->
+                                <!--<th class="text-right">Totals</th>-->
+                            <!--</tr>-->
+                            <!--</thead>-->
+                            <!--<tbody>-->
+                            <!--<tr>-->
+                                <!--<td class><strong>GFT Total Amount</strong></td>-->
+                                <!--<td v-for="data_gft in gft" class="text-right">{{ data_gft.total }}</td>-->
+                                <!--<td class="text-right">{{ sum(gft, 'total').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>GFT Count</td>-->
+                                <!--<td v-for="data_gft in gft" class="text-right">{{ data_gft.count}}</td>-->
+                                <!--<td class="text-right">{{ sum(gft, 'count') }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>GFT Avg</td>-->
+                                <!--<td v-for="data_gft in gft" class="text-right">{{ data_gft.avg}}</td>-->
+                                <!--<td class="text-right">{{ sum(gft, 'avg').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>This Month Total Amount</td>-->
+                                <!--<td v-for="data_thisMonth in thisMonth" class="text-right">{{ data_thisMonth.total}}-->
+                                <!--</td>-->
+                                <!--<td class="text-right">{{ sum(thisMonth, 'total').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>This Month Count</td>-->
+                                <!--<td v-for="data_thisMonth in thisMonth" class="text-right">{{ data_thisMonth.count}}-->
+                                <!--</td>-->
+                                <!--<td class="text-right">{{ sum(thisMonth, 'count') }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>This Month Avg</td>-->
+                                <!--<td v-for="data_thisMonth in thisMonth" class="text-right">{{ data_thisMonth.avg}}</td>-->
+                                <!--<td class="text-right">{{ sum(thisMonth, 'avg').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>30 Days Total Amount</td>-->
+                                <!--<td v-for="data_thirtyDays in thirtyDays" class="text-right">{{-->
+                                    <!--data_thirtyDays.total}}-->
+                                <!--</td>-->
+                                <!--<td class="text-right">{{ sum(thirtyDays, 'total').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>30 Days Count</td>-->
+                                <!--<td v-for="data_thirtyDays in thirtyDays" class="text-right">{{-->
+                                    <!--data_thirtyDays.count}}-->
+                                <!--</td>-->
+                                <!--<td class="text-right">{{ sum(thirtyDays, 'count') }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>30 Days Avg</td>-->
+                                <!--<td v-for="data_thirtyDays in thirtyDays" class="text-right">{{ data_thirtyDays.avg}}-->
+                                <!--</td>-->
+                                <!--<td class="text-right">{{ sum(thirtyDays, 'avg').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>All Total Amount</td>-->
+                                <!--<td v-for="data_all in all" class="text-right">{{ data_all.total}}</td>-->
+                                <!--<td class="text-right">{{ sum(all, 'total').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>All Count</td>-->
+                                <!--<td v-for="data_all in all" class="text-right">{{ data_all.count}}</td>-->
+                                <!--<td class="text-right">{{ sum(all, 'count') }}</td>-->
+                            <!--</tr>-->
+                            <!--<tr>-->
+                                <!--<td>All Avg</td>-->
+                                <!--<td v-for="data_all in all" class="text-right">{{ data_all.avg}}</td>-->
+                                <!--<td class="text-right">{{ sum(all, 'avg').toFixed(2) }}</td>-->
+                            <!--</tr>-->
+                            <!--</tbody>-->
+                        <!--</table>-->
+                    <!--</div>-->
+                <!--</div>-->
+            <!--</div>-->
+        <!--</div>-->
 
-        </div>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
+                    <div class="card-header card-header-flex">
                         <h4 class="card-title mb-0">Collection Hours</h4>
-                        <object align="right"><a href="/admin/collector-hours" class="btn btn-info btn-md">Go to
-                            Detailed Hours</a></object>
+                        <button class="btn btn-info rounded" @click="redirectCollectorHours()">Go to Detailed Hours</button>
                     </div>
                     <div class="card-body">
                         <table class="table table-responsive-sm table-striped">
                             <thead>
-                            <tr>
-                                <th>Collection Group</th>
-                                <th class="text-right">Number of Active Employees</th>
-                                <th class="text-right">Total Hours Worked</th>
-                                <th class="text-right">Current 30 Day</th>
-                                <th class="text-right">Current Goal</th>
-                            </tr>
+                                <tr>
+                                    <th>Collection Group</th>
+                                    <th class="text-right">Number of Active Employees</th>
+                                    <th class="text-right">Total Hours Worked</th>
+                                    <th class="text-right">Current 30 Day</th>
+                                    <th class="text-right">Current Goal</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            <tr v-for="item in collectionhours">
-                                <td v-for="(place, index) in item" :class="getClass(index)">{{ toNumber(place, index)
-                                    }}
-                                </td>
-                                <td class="text-right" v-if="item.name.match(/Unifin.*/)">{{ (item.time*125).toFixed(2)
-                                    }}
-                                </td>
-                                <td class="text-right" v-else>{{ (item.time*45).toFixed(2) }}</td>
-                            </tr>
+                                <tr v-for="item in collectionhours">
+                                    <td v-for="(place, index) in item" :class="getClass(index)">{{ toNumber(place, index)
+                                        }}
+                                    </td>
+                                    <td class="text-right" v-if="item.name.match(/Unifin.*/)">{{ (item.time*125).toFixed(2)
+                                        }}
+                                    </td>
+                                    <td class="text-right" v-else>{{ (item.time*45).toFixed(2) }}</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -296,6 +304,14 @@
 					'#0c0b10',
 				];
 			},
+
+            redirectTodaysTotals() {
+				window.location.href = '/admin/todays-totals';
+            },
+
+            redirectCollectorHours() {
+				window.location.href = '/admin/collector-hours';
+            }
 		},
 
 		computed: {
@@ -334,3 +350,11 @@
 		}
 	}
 </script>
+
+<style>
+    .card-header-flex {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
